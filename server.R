5d38a84d17b705617a166343332ac655f370e9e9
xtrangeloop <55667933+xtrangeloop@users.noreply.github.com>
2020-01-02 19:44:24 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -1,52 +1,39 @@
 library(shiny)
+library(ggplot2)
 
-shinyServer(function(input, output) {
-   mtcars$mpgsp <- ifelse(mtcars$mpg - 20 > 0, mtcars$mpg - 20, 0)
-   model1 <- lm(hp ~ mpg, data = mtcars)
-   model2 <- lm(hp ~ mpgsp + mpg, data = mtcars)
-   
-   model1pred <- reactive({
-       mpgInput <- input$sliderMPG
-       predict(model1, newdata = data.frame(mpg = mpgInput))
-   })
-   
-   model2pred <- reactive({
-       mpgInput <- input$sliderMPG
-       predict(model2, newdata = 
-                   data.frame(mpg = mpgInput,
-                              mpgsp = ifelse(mpgInput - 20 > 0,
-                                             mpgInput - 20, 0)))
-   })
+# in the following we give the function for caluclating parabola
+Calculate <- function(x, aDirection, xIntercept, yIntercept) {
+  y <- (aDirection*x^2) + (xIntercept * x) + (yIntercept) 
+  data.frame(x=x, y=y)
+}
+
+# Define server logic required to design our shinyapp
+serverLogic <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
     
-     output$plot1 = renderPlot({
-        mpgInput <- input$sliderMPG
-        plot(mtcars$mpg, mtcars$hp, xlab = "Miles per Gallon", 
-             ylab = "Horsepower", bty = "n", pch = 16,
-             xlim = c(10, 35), ylim = c(50, 350))
-        
-        if(input$showModel1){
-            abline(model1, col = "red", lwd = 2)
-        }
-        
-        if(input$showModel2){
-            model2lines <- predict(model2, newdata = data.frame(
-                mpg = 10:35, mpgsp = ifelse(10:35 - 20 > 0, 10:35 - 20, 0)
-            ))
-            lines(10:35, model2lines, col = "blue", lwd = 2)
-        }
-        
-        legend(25, 250, c("Model 1 Prediction", "Model 2 Prediction"), pch = 16, 
-               col = c("red", "blue"), bty = "n", cex = 1.2)
-        points(mpgInput, model1pred(), col = "red", pch = 16, cex = 2)
-        points(mpgInput, model2pred(), col = "blue", pch = 16, cex = 2)
-    })
-     
-     output$pred1 <- renderText({
-         model1pred()
-     })
-     
-     output$pred2 <- renderText({
-         model2pred()
-     })
-     
-})
+    xIntercept <- input$xIntercept
+    yIntercept <- input$yIntercept
+    aDirection <- input$aDirection
+    
+    baseParabola <- Calculate(seq(-10,10,by=0.1), aDirection = 1, xIntercept = 0, yIntercept = 0)
+    
+    xyGrid <- Calculate(0, aDirection = 1, xIntercept = 0, yIntercept = 0)
+    
+    newParabola <- Calculate(seq(-10,10,by=0.1), aDirection = aDirection, xIntercept = xIntercept, yIntercept = yIntercept)
+    
+    # draw the plot  
+    ggplot() +
+      ggtitle(paste0("y = ", aDirection, "x^2 + ", xIntercept, "x + ", yIntercept, ")")) + 
+      coord_cartesian(xlim = c(-10, 10), ylim = c(-10, 10)) +
+      geom_line(data=baseParabola, aes(x=x, y=y), size=2, color="black", alpha=0.3) + 
+      geom_vline(data=xyGrid, aes(xintercept = x), size=2, color="red", alpha=0.3) +
+      geom_hline(data=xyGrid, aes(yintercept=y), size=2, color="red", alpha=0.3) +
+      geom_line(data=newParabola, aes(x=x, y=y), size=1, color="blue")  
+  
+  }, height = 500, width = 500)
+  
+}
+
+# Add serverLogic
+shinyServer(serverLogic)
--- ui.R
+++ ui.R
@@ -1,32 +1,43 @@
 library(shiny)
 
-shinyUI(fluidPage(
-    titlePanel("Predict Horsepower from MPG"),
-    sidebarLayout(
-        sidebarPanel(
-            helpText("Learn about the interaction between horsepower and miles per gallon."),
-            br(),
-            sliderInput("sliderMPG", "What is the MPG of the car?", 
-                        10, 35, value = 20),
-            checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
-            checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
-            br(),
-                    
-            helpText("The whole code and data can ", 
-                             tags$a(href="https://github.com/xtrangeloop/Shiny", 
-                                    "be found here."),),
-            br(),
-            helpText("Author: xtrangeloop as part of the Coursera Data Science coursework,
-                     specifically in the class Developing Data Products.") 
-            
-            ),
-        mainPanel(
-            plotOutput("plot1"),
-            h3("Predicted Horsepower from Model 1:"),
-            textOutput("pred1"),
-            h3("Predicted Horsepower from Model 2:"),
-            textOutput("pred2")
 
-        )
+# Define UI for application
+ui <- fluidPage(
+  titlePanel("Exploring Quadratic Equations"),
+  
+  # Sidebar with a slider input
+  sidebarLayout(
+    sidebarPanel(
+      h3("Ax^2 + Bx + C"), 
+      br(),
+      helpText("Move the A, B, and C sliders to explore their interactions on the parabola."),
+      sliderInput("aDirection", "'A' determines the direction (up or down) and width.",
+                  min = -5, max = +5, step = .1, value = 1),
+      br(),
+      sliderInput("xIntercept", "'B' shifts the parabola along the x-axis (left and right).",
+                  min = -5, max = 5, step = 1, value = 0),
+      br(),
+      sliderInput("yIntercept", "'C' shifts the parabola along the y-axis (up and down).",
+                  min = -5, max = +5, step = 1, value = 0),
+      
+      br(),
+      
+      helpText("The whole code and data can ", 
+               tags$a(href="https://github.com/xtrangeloop/Shiny", 
+                      "be found here."),),
+      br(),
+      helpText("Author: xtrangeloop as part of the Coursera Data Science coursework,
+                     specifically in the class Developing Data Products.") 
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      h4("All quadratic equations form a U-shaped graph called a parabola. 
+         Use the sliders to the left to explore the resulting parabolas."),
+      helpText("What happens when you set A = 0?"),
+      plotOutput("distPlot")
     )
-))
+  )
+)
+
+
+# Draws UI
